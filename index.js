4edf237cb88be9b7773b152c125861609935e987
francisco-vasconcelos <francisco.vasconcelos@durham.ac.uk>
2019-02-02 21:24:02 +0000

    changed ports in index

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //const shost = require('./app');
 const host = require('./app');
 
-
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 //shost.listen(443);
-host.listen(80);
+host.listen(port, ipaddress);
